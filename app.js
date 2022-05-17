495b49f6ae6271aef5c6b841750297cadf3f62a0
Nasir Sayed <nasz@Nasirs-Mac-mini.local>
2022-05-16 22:04:52 -0400

    test

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.json({extended: false})); //This is the line that you want to add
-
-app.post('/postroute', (req, res) => {
-    console.log('body :', req.body);
+app.post('/post-test', (req, res) => {
+    console.log('Got body:', req.body);
     res.sendStatus(200);
 });
+
+app.listen(8080, () => console.log(`Started server at http://localhost:8080!`));
